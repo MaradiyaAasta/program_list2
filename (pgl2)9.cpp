f4873a6538a70e52f13c191d1a79aa13368e8ff8
Maradiya Asta <141497651+MaradiyaAasta@users.noreply.github.com>
2023-09-16 13:20:06 +0530

    Add files via upload

---
+++ (pgl2)9.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int no1;
+int main()
+{
+	int no2;
+	
+	cout<<"Enter the 1 st number ::";
+	cin>>no1;
+	
+	cout<<"Enter the 2 nd number ::";
+	cin>>no2;
+	
+	cout<<"Sum is ::"<<no1+no2;
+	
+	return 0;
+}
